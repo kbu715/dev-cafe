89aa8a61c3bb7f5d77a5f58e931c2c7d3e6e1ec7
kbu715 <kbu715@gmail.com>
2021-06-21 23:58:00 +0900

    ⚡️ 스타일 컴포넌트로 리렌더링 방지

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
 
 import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,7 +33,7 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item key="search">
-          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+          <SearchInput enterButton style={{ verticalAlign: 'middle' }} />
         </Menu.Item>
       </Menu>
       <Row gutter={16}>
