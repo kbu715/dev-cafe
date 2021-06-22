0c5cf5357a6348a6863d3f32b9efe6cb36e185ee
kbu715 <kbu715@gmail.com>
2021-06-22 12:00:28 +0900

    ✨ antd로 프로필 페이지 만들기

---
+++ prepare/front/components/FollowList.js
@@ -0,0 +1,39 @@
+import { Button, Card, List } from 'antd';
+import { MinusCircleOutlined } from '@ant-design/icons';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+const FollowList = ({ header, data }) => {
+  const listStyle = useMemo(() => ({ marginBottom: '20px' }));
+  const loadMoreStyle = useMemo(() => ({ textAlign: 'center', margin: '10px 0' }));
+  const listItemStyle = useMemo(() => ({ marginTop: '20px' }));
+  return (
+    <List
+      style={listStyle}
+      grid={{ gutter: 4, xs: 2, md: 3 }}
+      size="small"
+      header={<div>{header}</div>}
+      loadMore={
+        <div style={loadMoreStyle}>
+          <Button>더 보기</Button>
+        </div>
+      }
+      bordered
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item style={listItemStyle}>
+          <Card actions={[<MinusCircleOutlined key="minus" />]}>
+            <Card.Meta description={item.nickname} />
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+FollowList.propTypes = {
+  header: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+};
+
+export default FollowList;
--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -2,7 +2,13 @@ import React from 'react';
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
 
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
+  const followerList = [{ nickname: '짜장면' }, { nickname: '탕수육' }, { nickname: '팔보채' }];
+  const followingList = [{ nickname: '짬뽕' }, { nickname: '깐풍기' }, { nickname: '볶음밥' }];
+
   return (
     <>
       <Head>
@@ -10,7 +16,9 @@ const Profile = () => {
         <title>내 프로필 | Nodebird</title>
       </Head>
       <AppLayout>
-        <div>내 프로필</div>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
   );
