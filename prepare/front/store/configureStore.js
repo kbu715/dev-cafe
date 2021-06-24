a20b0efe23ec2e0b4b1a34d89e6b555b5d7b1563
kbu715 <kbu715@gmail.com>
2021-06-24 17:13:37 +0900

    🔥 Remove redux-thunk

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,21 +1,12 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { createWrapper } from 'next-redux-wrapper';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
 
 import reducer from '../reducers';
 
 const configureStore = () => {
-  const loggerMiddleware =
-    ({ dispatch, getState }) =>
-    (next) =>
-    (action) => {
-      console.log(action);
-      return next(action);
-    };
-
   // saga or thunk in array
-  const middlewares = [thunkMiddleware, loggerMiddleware];
+  const middlewares = [];
 
   // 중앙 데이터들이 어떻게 변하는지 다 보이기 때문에 보안에 취약할 수 있다.
   // 배포용일땐 DevTool 연결 하지 않는다.
