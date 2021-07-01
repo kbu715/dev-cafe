3a13b60ea35e03007a3ee07648558480edb49769
kbu715 <kbu715@gmail.com>
2021-07-01 17:03:41 +0900

    ✨ 시퀄라이즈가 자동으로 만들어준 메소드 예시들

--- prepare/back/models/post.js
+++ prepare/back/models/post.js
@@ -15,14 +15,16 @@ module.exports = (sequelize, DataTypes) => {
     },
   );
   Post.associate = (db) => {
-    db.Post.belongsTo(db.User);
-    db.Post.hasMany(db.Comment);
-    db.Post.hasMany(db.Image);
-    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+    db.Post.belongsTo(db.User); // post.addUser // post.getUser // post.setUser
+    db.Post.hasMany(db.Comment); // post.addComments
+    db.Post.hasMany(db.Image); // post.addImages // hasMany 라서  addImages
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' }); // post.addHashtags
     db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' }); // 게시글의 좋아요 와 사용자의 관계
     // 나중에 as에 따라서 post.getLikers처럼 게시글 좋아요 누른 사람을 가져오게 된다.
+    // post.addLikers, post.removeLikers 생긴다
     // as 별칭
     db.Post.belongsTo(db.Post, { as: 'Retweet' }); // 어떤 게시글이 어떤 게시글의 리트윗 게시글
+    // post.addRetweet
   };
   return Post;
 };
