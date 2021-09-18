47dcbc6bd5f596129a648cbcd945036976aa1ae6
강바울 <kbu715@naver.com>
2021-09-18 15:28:45 +0900

    ✨ Set Next.js document file and web font link

---
+++ prepare/front/pages/_document.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="ko-KR">
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
