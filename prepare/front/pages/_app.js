bd94682c6c08b1a6881e4d633a15d788e1429056
kbu715 <kbu715@gmail.com>
2021-09-19 21:35:54 +0900

    💄 Add global styles

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
-
 import wrapper from '../store/configureStore';
+import { GlobalStyle } from '../global-styles';
 
 const App = ({ Component }) => {
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
-        <title>Nodebird</title>
+        <title>타이틀</title>
       </Head>
+      <GlobalStyle />
       <Component />
     </>
   );
