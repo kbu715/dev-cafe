ee330e53a9d5ec5b8ae81677704a38f7c2ae6ea0
kbu715 <kbu715@gmail.com>
2021-09-20 15:53:31 +0900

    ✨ Implement global footer and contact modal component

---
+++ prepare/front/components/Footer/ContactModal.js
@@ -0,0 +1,87 @@
+import React, { useContext } from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { ThemeContext } from '../../pages/_app';
+import { GFOOTER_Z_INDEX } from '../../utils/constant';
+
+const ModalContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: ${GFOOTER_Z_INDEX};
+`;
+
+const ContactWrapper = styled.div`
+  width: 250px;
+  height: 150px;
+  border-radius: 8px;
+  background: ${(props) => props.theme.itemBackground};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const ModalHeader = styled.header`
+  width: 100%;
+  padding: 0 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 17px;
+  font-weight: 600;
+`;
+
+const ModalContent = styled.div`
+  width: 100%;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  & div {
+    margin-left: 20px;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const ContactModal = ({ trigger }) => {
+  const { theme } = useContext(ThemeContext);
+  const onMaskClick = (e) => {
+    if (e.target === e.currentTarget) {
+      trigger();
+    }
+  };
+
+  return (
+    <ModalContainer onClick={onMaskClick}>
+      <ContactWrapper theme={theme}>
+        <ModalHeader>
+          <span>Contact</span>
+        </ModalHeader>
+        <ModalContent>
+          <AiOutlineMail size={20} />
+          <div>
+            <address>
+              <a href="mailto:kbu715@gmail.com">kbu715@gmail.com</a>
+            </address>
+          </div>
+        </ModalContent>
+      </ContactWrapper>
+    </ModalContainer>
+  );
+};
+
+ContactModal.propTypes = {
+  trigger: PropTypes.func.isRequired,
+};
+
+export default ContactModal;
---
+++ prepare/front/components/Footer/GlobalFooter.js
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from 'react';
+import styled from 'styled-components';
+import { AiOutlineGithub, AiOutlineMail } from 'react-icons/ai';
+import { lightTheme } from '../../theme';
+import { ThemeContext } from '../../pages/_app';
+import ContactModal from './ContactModal';
+
+const Footer = styled.footer`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid ${(props) => (props.theme === lightTheme ? '#bfbac5cc' : '#7b7b7b')};
+  padding: 0 55px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FooterLogoWrapper = styled.div`
+  font-size: 1.1rem;
+  font-weight: 700;
+  font-family: 'Comfortaa', cursive;
+  margin-right: 5px;
+  color: ${(props) => (props.theme === lightTheme ? '#a0a0a0' : '#7b7b7b')};
+`;
+
+const FooterIconWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  & svg {
+    color: ${(props) => (props.theme === lightTheme ? '#a0a0a0' : '#7b7b7b')};
+    &:hover {
+      color: ${(props) => (props.theme === lightTheme ? '#363537' : '#fafafa')};
+    }
+  }
+`;
+
+const Icon = styled.div`
+  margin: 0 10px;
+  cursor: pointer;
+`;
+
+const GlobalFooter = () => {
+  const [contact, setContact] = useState(false);
+  const { theme } = useContext(ThemeContext);
+
+  const contactTrigger = () => setContact((prev) => !prev);
+  return (
+    <>
+      <Footer theme={theme}>
+        {/* TODO: 나중에 로고로 바꿀까? */}
+        <FooterLogoWrapper theme={theme}>Dev-Cafe</FooterLogoWrapper>
+        <FooterIconWrapper theme={theme}>
+          <Icon>
+            <AiOutlineGithub size={28} onClick={() => window.open(`https://github.com/kbu715`, '_blank')} />
+          </Icon>
+          <Icon>
+            <AiOutlineMail size={28} onClick={contactTrigger} />
+          </Icon>
+        </FooterIconWrapper>
+      </Footer>
+      {contact && <ContactModal trigger={contactTrigger} />}
+    </>
+  );
+};
+
+export default GlobalFooter;
