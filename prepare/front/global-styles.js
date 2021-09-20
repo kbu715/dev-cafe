fa030ca212d0a4b137a9f3a3ac48c73a93fb9f3b
kbu715 <kbu715@gmail.com>
2021-09-20 15:54:01 +0900

    ♻️ Update reset style

--- prepare/front/global-styles.js
+++ prepare/front/global-styles.js
@@ -5,6 +5,9 @@ export const GlobalStyle = createGlobalStyle`
     ${reset}
     * {
         box-sizing: border-box;
+        margin: 0;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
     }
     body {
         font-family: 'Noto Sans', sans-serif;
@@ -12,20 +15,45 @@ export const GlobalStyle = createGlobalStyle`
         color: ${({ theme }) => theme.text};
         transition: all 0.5s ease-in-out;
     }
-    button { 
-        cursor: pointer;
-        border: none;
-        outline: none;
-    }
-    ol, ul, li {
-        list-style: none;
-    }
-    a {
-        text-decoration: none;
-        cursor: pointer;
-    }
-    img {
-        width: 100%;
-        height: 100%;
-    }
+    
+h1 {
+  margin: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+button,
+input,
+select,
+textarea {
+  background-color: transparent;
+  border: 0;
+
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+}
+
+a,
+button {
+  cursor: pointer;
+}
+
+button {
+  padding: 0;
+}
+
+ul,
+ol {
+  padding-left: 0;
+  list-style: none;
+}
+
+address {
+  font-style: normal;
+}
 `;
