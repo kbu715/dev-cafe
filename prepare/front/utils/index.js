5904df10ae6ea9d2ca6d6a21e2433a71ec978608
kbu715 <kbu715@gmail.com>
2021-09-21 17:35:46 +0900

    ✨ utils

---
+++ prepare/front/utils/index.js
@@ -0,0 +1,5 @@
+export const getRandomIndex = (n) => {
+  return Math.floor(Math.random() * n);
+};
+
+export const backgroundImages = ['assets/images/main01.jpg', 'assets/images/main02.jpg', 'assets/images/main03.jpg'];
