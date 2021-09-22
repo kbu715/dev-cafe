0458dbaf9a944474675da7a568e07c8e20cc80ee
kbu715 <kbu715@gmail.com>
2021-09-22 16:31:35 +0900

    ✨ Prepare for SSR and getServerSideProps

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { END } from 'redux-saga';
 import AppLayout from '../components/AppLayout';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
 import { LOAD_POSTS_REQUEST } from '../reducers/post';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
+import wrapper from '../store/configureStore';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,15 +18,6 @@ const Home = () => {
       alert(retweetError);
     }
   }, [retweetError]);
-
-  useEffect(() => {
-    dispatch({
-      type: LOAD_MY_INFO_REQUEST,
-    });
-    dispatch({
-      type: LOAD_POSTS_REQUEST, // 게시글 불러오기
-    });
-  }, []);
 
   useEffect(() => {
     function onScroll() {
@@ -58,4 +50,26 @@ const Home = () => {
   );
 };
 
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  // console.log(context);
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  context.store.dispatch({
+    type: LOAD_POSTS_REQUEST,
+  });
+  context.store.dispatch(END);
+
+  await context.store.sagaTask.toPromise();
+});
+
 export default Home;
+
+// useEffect(() => {
+//   dispatch({
+//     type: LOAD_MY_INFO_REQUEST,
+//   });
+//   dispatch({
+//     type: LOAD_POSTS_REQUEST, // 게시글 불러오기
+//   });
+// }, []);
