c5b4d53e4eb6789850225662b4f5a3768f1b61de
kbu715 <kbu715@gmail.com>
2021-09-22 17:59:45 +0900

    ♿️ polyfill setting for I.E

--- prepare/front/pages/_document.js
+++ prepare/front/pages/_document.js
@@ -37,6 +37,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019" />
           <Main />
           <NextScript />
         </body>
