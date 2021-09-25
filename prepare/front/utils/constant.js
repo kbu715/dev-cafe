b8239b234f979aa113d59b63ff541b986f36a374
kbu715 <kbu715@gmail.com>
2021-09-25 13:18:34 +0900

    ♻️ add z-index constants

--- prepare/front/utils/constant.js
+++ prepare/front/utils/constant.js
@@ -1,4 +1,6 @@
 export const GNB_HEIGHT = 70;
 export const GNB_Z_INDEX = 100;
 export const GFOOTER_Z_INDEX = 200;
-export const MODAL_Z_INDEX = 300;
+export const SCROLLTOTOPBTN_Z_INDEX_ACTIVE = 300;
+export const SCROLLTOTOPBTN_Z_INDEX_INACTIVE = -100;
+export const MODAL_Z_INDEX = 400;
