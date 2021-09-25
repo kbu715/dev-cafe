5eeef3e76610760d0d399b140d6f637ef0138161
kbu715 <kbu715@gmail.com>
2021-09-25 18:54:39 +0900

    ⚰️ Delete: unnecessary code

--- prepare/back/models/user.js
+++ prepare/back/models/user.js
@@ -28,7 +28,6 @@ module.exports = class User extends Model {
   }
   static associate(db) {
     db.User.hasMany(db.Post);
-    db.User.hasMany(db.Report);
     db.User.hasMany(db.Comment);
     db.User.belongsToMany(db.Post, { through: 'Like', as: 'Liked' })
     db.User.belongsToMany(db.User, { through: 'Follow', as: 'Followers', foreignKey: 'FollowingId' });
